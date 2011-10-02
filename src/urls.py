3dd9b4b133d2fdbea160521a27025ef0a503fe9f
Alexander Milano <amilano@stcsolutions.com.ve>
2011-10-02 10:32:26 -0430

    Probando lastfm api

--- src/urls.py
+++ src/urls.py
@@ -5,6 +5,7 @@ from tipfy import Rule
 rules = [
     Rule('/', endpoint='home', handler='handlers.UserHandler.HomeHandler'),
     Rule('/auth/login', endpoint='auth/login', handler='handlers.UserHandler.LoginHandler'),
+    Rule('/artist', endpoint='artist', handler='handlers.LastFmHandler.LastFmSearchHandler'),
     Rule('/auth/logout', endpoint='auth/logout', handler='handlers.UserHandler.LogoutHandler'),
     Rule('/auth/signup', endpoint='auth/signup', handler='handlers.UserHandler.SignupHandler'),
     Rule('/auth/register', endpoint='auth/register', handler='handlers.UserHandler.RegisterHandler'),
